1e8a5c64c8ec071a595403c446e8152a656c8955
Raphael Berendes <rbe.extern@starfinanz.de>
2019-11-25 15:04:26 +0100

    Initial Commit

---
+++ ZipPlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZipPlayground
+//
+//  Created by Raphael Berendes (extern) on 25.11.19.
+//  Copyright © 2019 Raphael Berendes (extern). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
